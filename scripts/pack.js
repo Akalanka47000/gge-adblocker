035d13e10f6847bf708689336fdd3d2c8d03752f
Akalanka <akalankaperera128@gmail.com>
2023-01-31 22:45:03 +0530

    Updated build workflow

--- scripts/pack.js
+++ scripts/pack.js
@@ -1,6 +1,7 @@
 const crx3 = require('crx3');
 
 crx3(['./src/manifest.json'], {
+  keyPath: './key.pem',
   crxPath: `./build/gge-adblocker.crx`,
   zipPath: `./build/gge-adblocker.zip`,
 }).then(() => console.log('Extension packed successfully')).catch((err) => console.log(`Error packing extension: ${err}`))
